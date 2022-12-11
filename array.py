42484dd97f65de3197120381e101491e0de37e3a
weberaidanempey <113483344+weberaidanempey@users.noreply.github.com>
2022-12-10 18:37:12 -0600

    first attempt

---
+++ array.py
@@ -0,0 +1,59 @@
+#problem 1
+    #variables
+my_first = str("Aidan")
+my_last = str("Empey")
+print("Print your name: ", my_first , my_last );
+
+#problem 2
+car_lists = ['Ford', 'Chrysler', 'Dodge', 'Ram','Jeep', 'Chevy', 'GMC']
+
+#problem 3
+print(car_lists)
+
+#problem 4
+print(len(car_lists))
+
+#problem 5
+car_lists.append('Buick')
+
+#problem 6
+print(car_lists)
+
+#problem 7
+print(car_lists[3:4])
+
+#problem 8 
+car_lists.insert(3,'Toyota')
+
+#problem 9
+print(car_lists)
+
+#problem 10
+car_lists.pop(5)
+
+#problem 11
+print(car_lists)
+
+#problem 12
+car_lists.sort()
+
+#problem 13
+print(car_lists)
+
+#problem 14
+car_lists.sort(reverse = True)
+
+#problem 15
+print(car_lists)
+
+#problem 16
+my_array_length = car_lists
+print(len(my_array_length))
+
+#problem 17
+array_string = car_lists
+print (len("The length of my array is "))
+
+#problem 18
+print(array_string)
+print(my_array_length)
